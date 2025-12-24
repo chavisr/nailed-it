3b91aa1a56eb50c3e6119f7188690335edd228c6
Chavis Rujichan <chavis.rujichan@gmail.com>
2025-12-24 12:19:03 +0700

    Make grid default color brighter
    - Changed grid default color from #444444 (dark gray) to #888888 (medium gray)
    - Updated DEFAULT_CANVAS gridColor value
    - Updated fallback value in grid rendering code
    - Updated fallback value in grid color picker
    - Grid is now much more visible against dark backgrounds
    - Approximately 2x brighter than previous default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,5 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {
-      colors: {
-        gruvbox: {
-          bg0: '#1d2021',
-          bg1: '#282828',
-          bg2: '#32302f',
-          bg3: '#3c3836',
-          bg4: '#45403d',
-          bg5: '#504945',
-          fg: '#d4be98',
-          red: '#ea6962',
-          green: '#a9b665',
-          yellow: '#d8a657',
-          blue: '#7daea3',
-          purple: '#d3869b',
-          aqua: '#89b482',
-          orange: '#e78a4e',
-          gray: '#928374',
-        }
-      }
-    }
-  },
+  theme: { extend: {} },
   plugins: [],
 }
